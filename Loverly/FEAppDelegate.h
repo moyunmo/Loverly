d8a92da0318c3bf99fd870f244143045299fcdf6
Moyun <momo605654602@gmail.com>
2014-06-26 13:35:40 +0800

    Initial Commit

---
+++ Loverly/FEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FEAppDelegate.h
+//  Loverly
+//
+//  Created by Moyun on 14/6/26.
+//  Copyright (c) 2014年 Moyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Loverly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Loverly
+//
+//  Created by Moyun on 14/6/26.
+//  Copyright (c) 2014年 Moyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FEAppDelegate class]));
+    }
+}
